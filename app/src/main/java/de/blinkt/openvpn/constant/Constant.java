d37859bc243a1c49ee2d9fbb3dc2e18650a719ca
xiaoweif <727543778@qq.com>
2017-02-17 14:06:42 +0800

    修改上电断电指令

--- app/src/main/java/de/blinkt/openvpn/constant/Constant.java
+++ app/src/main/java/de/blinkt/openvpn/constant/Constant.java
@@ -58,10 +58,10 @@ public class Constant {
 	public static final String CALL_OUTGOING = "拨出";
 	public static final String CALL_MISSED = "未接";
 	//蓝牙指令
-	public static String UP_TO_POWER = "AADB040174";//上电指令，没有详细卡信息以及BB332211BB
-	public static String UP_TO_POWER_DETAIL = "AADB040277";//上电指令,只有详细卡信息以及BB665544BB
-	public static String UP_TO_POWER_USED_TO_SDK = "AADB040376";//上电指令,返回BB998877BB，用于SDK使用的上下电命令
-	public static final String OFF_TO_POWER = "AADC040173";//断点指令
+	public static String UP_TO_POWER = "888003100001";//上电指令，没有详细卡信息以及BB332211BB
+	public static String UP_TO_POWER_DETAIL = "888003100002";//上电指令,只有详细卡信息以及BB665544BB
+	public static String UP_TO_POWER_USED_TO_SDK = "888003100003";//上电指令,返回BB998877BB，用于SDK使用的上下电命令
+	public static final String OFF_TO_POWER = "8880021100";//断电指令
 	public static final String REAL_TIME_STEPS = "AA010401AE ";//实时步数
 	public static final String HISTORICAL_STEPS = "AA030401AC";//历史步数
 	public static final String FIND_DEVICE = "AA060401A9";//查找手环
