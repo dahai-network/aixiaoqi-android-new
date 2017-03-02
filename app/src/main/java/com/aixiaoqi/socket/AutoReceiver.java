026b2479b801359103032e92ee1e82ca2c1fc090
w441813542 <441813542@qq.com>
2017-03-02 09:42:40 +0800

    日常提交

--- app/src/main/java/com/aixiaoqi/socket/AutoReceiver.java
+++ app/src/main/java/com/aixiaoqi/socket/AutoReceiver.java
@@ -3,31 +3,40 @@ package com.aixiaoqi.socket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.util.Log;
 
 import de.blinkt.openvpn.util.DateUtils;
+import de.blinkt.openvpn.util.NetworkUtils;
 
+import static android.content.Context.POWER_SERVICE;
 import static com.aixiaoqi.socket.SocketConstant.HEARTBEAT_PACKET_TIMER;
 
 /**
  * Created by Administrator on 2017/1/20 0020.
  * 系统定时发送心跳包
- *
  */
 
 public class AutoReceiver extends BroadcastReceiver {
 
 	private String TAG = "AutoReceiver";
+	public static PowerManager t_power;
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(final Context context, Intent intent) {
 
 		if (intent.getAction().equals(HEARTBEAT_PACKET_TIMER)) {
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
 					if (TestProvider.sendYiZhengService != null) {
-						Log.i(TAG, DateUtils.getCurrentDateForFileDetail()+" 发送心跳包");
+						if (t_power == null) {
+							t_power = (PowerManager) context
+									.getSystemService(POWER_SERVICE);
+							PowerManager.WakeLock t_wakelock = t_power.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SleepMonitor");
+							t_wakelock.acquire();
+						}
+						Log.i(TAG, DateUtils.getCurrentDateForFileDetail() + " 发送心跳包，是否联网：" + NetworkUtils.isNetworkAvailable(context));
 						TestProvider.sendYiZhengService.sendGoip(SocketConstant.UPDATE_CONNECTION);
 					} else {
 						Log.e(TAG, "AutoReceiver 异常！");
